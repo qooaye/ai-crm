d1ddc25488e9ffbfc732825686757f70176ae258
qooaye <qooaye03160617@gmail.com>
2026-01-01 02:35:53 +0800

    Remove .next build artifacts from git

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/jhe-jhihjhang/Desktop/qooaye/vibeCodingCamp/vibe-coding-9999/ai-crm/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
